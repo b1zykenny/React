00372dc3ca05dee58af627de60237e2d3d382ce3
Ruslan <92989534+b1zykenny@users.noreply.github.com>
2022-04-01 15:08:50 +0200

    HomeWork #3
    1. Установил material-ui. Переделал форму из HomeWork #2 с использованием компонентов из material-ui.
    2. Добавил автофокус на текстовое поле при открытии страницы
    3. Добавил “массив чатов” - массив объектов с полями name и id. Добавил список чатов из material-ui (список пока не несет никакой функциональности)

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,11 @@
+import Avatar from '@mui/material/Avatar';
+
 const Message = ({ message }) => {
     return (
         <div className="message">
-            <p> {message.author}</p>
+            <div className="messageZone">
+                <Avatar alt="User" src="/static/images/avatar/1.jpg" /><p> {message.author}</p>
+            </div>
             <blockquote>{message.textMessage}</blockquote>
         </div>
     );
